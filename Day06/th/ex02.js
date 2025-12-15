b5f7a250c92afb83051f0d48009fe24e4b6ff418
hoangnm-ndm <hoangnm.ndm@gmail.com>
2025-12-15 22:05:11 +0700

    Day 06: BTVN

---
+++ Day06/th/ex02.js
@@ -0,0 +1,17 @@
+let h1 = +window.prompt("nhap h1");
+let m1 = +window.prompt("nhap h1");
+let h2 = +window.prompt("nhap h2");
+let m2 = +window.prompt("nhap m2");
+
+/**
+ * * Bước 1: mời người dùng nhập và kiểm tra dữ liệu từ người dùng.
+ * * Bước 2: kiểm tra h1 > h2
+ *  - h1 > h2 -> kết luận true
+ *  - h1 < h2 -> kết luận false
+ *  - h1 = h2 -> kiểm tra m1 và m2.
+ */
+
+let thoitiet = "mua";
+
+console.log(thoitiet === "mua" ? "nghi hoc" : "di hoc");
+//* Toán tử 3 ngôi.
