2ca1325eae12200e45d0c191d37c27a3f78d5d54
hoangnm-ndm <hoangnm.ndm@gmail.com>
2025-12-15 21:50:30 +0700

    Day 06: Math

---
+++ Day06/th/ex01.js
@@ -0,0 +1,23 @@
+const myInfor = {
+  fullname: "Nguyen Minh Hoang",
+  age: 33,
+};
+
+console.log(myInfor);
+console.log(Math);
+
+console.log(Math.sqrt(2)); // can bac 2
+
+console.log(Math.random()); // ngau nhien tu 0 - 1
+console.log(Math.min(1, 4, 5, 10, -10, 100));
+console.log(Math.max(1, 4, 5, 10, -10, 100));
+console.log(Math.round(10.5));
+console.log(Math.round(-10.5)); // lam tron toi so nguyen gan nhat
+console.log(Math.ceil(10.1)); // lam tron len
+console.log(Math.floor(10.9)); // lam tron xuong
+console.log(Math.trunc(10.9)); // cat di phan thap phan
+
+// ! FRCT
+
+console.log(Math.pow(2, 4));
+console.log(2 ** 4);
